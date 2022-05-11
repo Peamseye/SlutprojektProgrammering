ce21c7729c17681e8c48f9b068ca41f08d5f4d1a
Peamseye <aispotato587@gmail.com>
2022-05-11 19:42:33 +0200

    124567

--- SlutprojektProgrammering/Program.cs
+++ SlutprojektProgrammering/Program.cs
@@ -1,26 +1,15 @@
 ﻿using System;
 using System.Numerics;
-using Raylib_cs;
-
-Texture2D ArenaImage = Raylib.LoadTexture("FinalProjBackground.png");
-
-//Spelfönster
-Raylib.InitWindow(800, 600, "Final project");
-Raylib.SetTargetFPS(60);
-
-//METOD
-//PSEUDOKOD
-//ARRAY / LISTA
-//ALGORITMER
-
-//FOR-LOOP
-//IF-ELSE
-//TRYPARSE
 
 
-while (!Raylib.WindowShouldClose())
-{
-    Raylib.BeginDrawing();
+//-Typkonvertering
+//-While loopar (starta om spelet / vinna spelet när passet är köpt).
+//-If-satser
+//-Array / Lista
+//-Random generator
+//-Metod
+//-Flera loopar (Spelet i sig + utmaning)
+//-Algoritm
 
-    Raylib.EndDrawing();
-}
+
+Console.WriteLine();
