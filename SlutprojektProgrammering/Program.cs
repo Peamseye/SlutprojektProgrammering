acff65ed5afc0ce7b52ede726a7df1492357c532
Peamseye <aispotato587@gmail.com>
2022-05-11 20:41:43 +0200

    1234567

--- SlutprojektProgrammering/Program.cs
+++ SlutprojektProgrammering/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Numerics;
 
-
 //-Typkonvertering
 //-While loopar (starta om spelet / vinna spelet när passet är köpt).
 //-If-satser
@@ -11,12 +10,41 @@ using System.Numerics;
 //-Flera loopar (Spelet i sig + utmaning)
 //-Algoritm
 
-//Programmets loop. Tillåter användaren att starta om loopen för att köpa fler produkter
+
+int currency = 200;
+//Startvaluta
+
+int amount = 0;
+//Mängden produkter i affären
+
+int price = 0;
+//Värdet på produkterna spelaren har valt att köpa
+
 bool game = true;
 
+//Programmets loop. Fortsätter loopas tills spelaren köper ett pass.
 while (game == true)
 {
-    
+
+//Fruit = Första svaret på frågan
+    string StoreOption = Console.ReadLine();
+    StoreOption = StoreOption.ToLower();
+
+    //Tvingar användaren att välja mellan a, b eller c.
+    while (StoreOption != "a" && StoreOption != "b" && StoreOption != "c")
+    {
+        Console.Clear();
+
+        Console.WriteLine($"You have {currency}$.");
+        Console.WriteLine("");
+        Console.WriteLine("What do you want to purchase?");
+        Console.WriteLine("");
+        Console.WriteLine("a = Steroids (50$)     b = Gloves (100$)     c = Pass (400$)");
+
+        StoreOption = Console.ReadLine();
+    }
+
+
 }
 
 Console.WriteLine();
--- SlutprojektProgrammering/Store.cs
+++ SlutprojektProgrammering/Store.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Store
 {
-    public static void WriteCentered(string text)
+    public static void Options(string text)
     {
 
     }
